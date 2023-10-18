b4c99d08fc13b6e39e95bea22b0c30ee81e48ab7
Brad Cole <artwired@users.noreply.github.com>
2023-10-18 14:43:14 -0700

    added subjective paradise modal

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const dnbDescriptionBtn = document.getElementById("dnb-description-btn");
 const dnbCloseBtn = document.getElementById("dnb-close-btn");
+const subjectiveDescriptionBtn = document.getElementById(
+  "subjective-description-btn"
+);
+const subjectiveCloseBtn = document.getElementById("subjective-close-btn");
 
 dnbDescriptionBtn.addEventListener("click", function () {
   document.getElementById("dnb-modal-container").classList.remove("hidden");
 });
 dnbCloseBtn.addEventListener("click", function () {
   document.getElementById("dnb-modal-container").classList.add("hidden");
+});
+
+subjectiveDescriptionBtn.addEventListener("click", function () {
+  document
+    .getElementById("subjective-modal-container")
+    .classList.remove("hidden");
+});
+subjectiveCloseBtn.addEventListener("click", function () {
+  document.getElementById("subjective-modal-container").classList.add("hidden");
 });

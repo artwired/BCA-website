b528a5f8b3a4159d1defe3066d1ae4f798f301ea
Brad Cole <artwired@users.noreply.github.com>
2023-10-18 14:05:03 -0700

    modal interactivity
    working on modal interactivity

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const dnbDescriptionBtn = document.getElementById("dnb-description-btn");
+const dnbCloseBtn = document.getElementById("dnb-close-btn");
 
 dnbDescriptionBtn.addEventListener("click", function () {
-  console.log("working");
+  document.getElementById("dnb-modal-container").classList.remove("hidden");
+});
+dnbCloseBtn.addEventListener("click", function () {
+  document.getElementById("dnb-modal-container").classList.add("hidden");
 });
